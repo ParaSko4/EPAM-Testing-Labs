6ebaeb286d3e08ff3a9421aa10810e5618e4df34
Gleb Shatrauka <gleb.shatrauka@gmail.com>
2019-11-26 15:38:14 +0300

    pageobject for webdriver tests momondo.by

---
+++ Lab_6/PageObject/WebDriverLib/Pages/FlightsPage.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+using OpenQA.Selenium;
+
+namespace PageObject.Pages
+{
+    public class FlightsPage
+    {
+        private readonly IWebDriver driver;
+        private bool check_Page = false;
+
+        private IWebElement input_From
+        {
+            get
+            {
+                if (check_Page)
+                {
+                    return FindElement("div > input", "-origin-airport");
+                }
+                else
+                {
+                    return FindElement("div > input", "-origin");
+                }
+            }
+        }
+
+        private IWebElement input_To
+        {
+            get
+            {
+                if (check_Page)
+                {
+                    return FindElement("div > input", "destination-airport");
+                }
+                else
+                {
+                    return FindElement("div > input", "destination");
+                }
+            }
+        }
+
+        private IWebElement display_inputFrom
+        {
+            get
+            {
+                return FindElement("div", "-origin-airport-display");
+            }
+        }
+
+        private IWebElement display_inputTo
+        {
+            get
+            {
+                return FindElement("div", "destination-airport-display");
+            }
+        }
+
+        private IWebElement body
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector("Body"));
+            }
+        }
+
+        private IWebElement button_Search
+        {
+            get
+            {
+                if (check_Page)
+                {
+                    return FindElement("div", "col-button-wrapper").FindElement(By.CssSelector("button"));
+                }
+                else
+                {
+                    return driver.FindElement(By.ClassName("buttonBlock")).FindElement(By.CssSelector("button"));
+                }
+            }
+        }
+
+        public string get_messageError
+        {
+            get
+            {
+                return FindElement("ul", "messages").FindElement(By.XPath("li/ul/li")).Text;
+            }
+        }
+
+        public FlightsPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            if (FindElement("div > input", "-origin") == null)
+            {
+                check_Page = true;
+            }
+        }
+
+
+        public void InputFromValue(string value)
+        {
+            if (check_Page)
+            {
+                display_inputFrom.Click();
+            }
+            else
+            {
+                input_From.Click();
+            }
+
+            input_From.SendKeys(value);
+            input_From.Click();
+        }
+
+        public void InputToValue(string value)
+        {
+            if (check_Page)
+            {
+                body.Click();
+                display_inputTo.Click();
+                input_To.SendKeys("Стамбул");
+
+                display_inputFrom.Click();
+            }
+            else
+            {
+                input_To.Click();
+                input_To.SendKeys("Стамбул");
+
+                input_From.Click();
+            }
+        }
+
+        public void PressSearch()
+        {
+            button_Search.Click();
+        }
+
+        private IWebElement FindElement(string attribute, string name)
+        {
+            ReadOnlyCollection<IWebElement> elements = driver.FindElements(By.CssSelector(attribute));
+            Regex findByCharptName = new Regex($@"\w*{name}$");
+
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (findByCharptName.Match(elements[i].GetAttribute("id")).Success && elements[i].Enabled)
+                {
+                    return elements[i];
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Lab_6/PageObject/WebDriverLib/Tests/NUnitHotelAndFlightsTest.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using PageObject.Pages;
+
+namespace PageObject
+{
+    [TestFixture]
+    public class FirefoxRequests
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void OpenBrouser()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.momondo.by";
+        }
+
+        [Test]
+        public void RequestValidation()
+        {
+            Assert.AreEqual(LogicRequestValidationTest(), "Пожалуйста, задайте конкретные аэропорты отправления (Откуда) и прибытия (Куда).");
+        }
+
+        [Test]
+        public void HotelRecommendation()
+        {
+            Assert.AreEqual(LogicHotelRecommendationTest(), "Поиск 8 и более номеров на HotelPlanner.com");
+        }
+
+        [TearDown]
+        public void CloseBrouser()
+        {
+            driver.Close();
+        }
+
+        private string LogicRequestValidationTest()
+        {
+            FlightsPage flights = new FlightsPage(driver);
+
+            string text_Input = "Стамбул";
+
+            flights.InputFromValue(text_Input);
+            flights.InputToValue(text_Input);
+            flights.PressSearch();
+
+            return flights.get_messageError;
+        }
+
+        private string LogicHotelRecommendationTest()
+        {
+            HotelPage hotel = new HotelPage(driver);
+            hotel.OpenHotelPage();
+            hotel.IncreaseNumberRooms();
+
+            return hotel.get_messageHotelAdvice;
+        }
+    }
+}
